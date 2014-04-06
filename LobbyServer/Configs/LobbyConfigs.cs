6c956425bcf798d52f7319c6396c6e7bb84dd8a9
Max Hayman <maxhayman@maxhayman.co.uk>
2014-04-06 17:56:47 +0100

    -Config files. They will be generated once you launch the server for the first time.

--- LobbyServer/Configs/LobbyConfigs.cs
+++ LobbyServer/Configs/LobbyConfigs.cs
@@ -12,8 +12,8 @@ namespace LobbyServer
     [aConfigAttributes("Configs/Lobby.xml")]
     public class LobbyConfigs : aConfig
     {
-        public int ClientPort = 8040;
-        public string ClientVersion = "1.3.5";
+        public int ClientPort = 8048;
+        public string ClientVersion = "1.4.8";
 
         public RpcClientConfig RpcInfo = new RpcClientConfig("127.0.0.1", "127.0.0.1", 6800);
         public LogInfo LogLevel = new LogInfo();
