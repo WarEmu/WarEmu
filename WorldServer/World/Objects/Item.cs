a6367d289ed6f8ac444f81e958ca738ba8a36048
Max Hayman <maxhayman@maxhayman.co.uk>
2014-05-06 20:46:15 +0100

    *fixed bugged trophy's which caused a client crash (fixed WarEmu/WarBugs #28 and #19)

--- WorldServer/World/Objects/Item.cs
+++ WorldServer/World/Objects/Item.cs
@@ -1,4 +1,24 @@
-﻿
+﻿/*
+ * Copyright (C) 2014 WarEmu
+ *	http://WarEmu.com
+ * 
+ * Copyright (C) 2011-2013 APS
+ *	http://AllPrivateServer.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -182,6 +202,14 @@ namespace WorldServer
             Out.WriteByte(Info.Rarity);
             Out.WriteByte(Info.Bind);
             Out.WriteByte(Info.Race);
+
+            // Trophys have some extra bytes
+            if (Info.Type == (byte)GameData.ItemTypes.ITEMTYPES_TROPHY)
+            {
+                Out.WriteUInt32(0);
+                Out.WriteUInt16(0x0080);
+            }
+
             Out.WriteUInt32(Info.Career);
             Out.WriteUInt32(0);
             Out.WriteUInt32(Info.SellPrice);
