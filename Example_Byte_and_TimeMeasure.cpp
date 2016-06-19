75c74b25dab73422719ba2cdd239ed90dce5fce9
CJ <zergienator@web.de>
2016-06-19 13:35:13 +0200

    update Example_Byte_and_TimeMeasure

--- Example_Byte_and_TimeMeasure.cpp
+++ Example_Byte_and_TimeMeasure.cpp
@@ -23,13 +23,6 @@ struct Byte
         return out << (int)b.mByte;
     }
 };
-
-unsigned long long
-GetCurrentMS()
-{
-    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-}
-
 //usage example:
 //
 //  {
@@ -41,17 +34,69 @@ GetCurrentMS()
 //
 struct TimeMeasure
 {
-    unsigned long long mStart;
-    
-    TimeMeasure()
+    enum UNIT
     {
-        this->mStart = this->GetCurrentMS();
+        INVALIDE = 0,
+        
+        MILLIESECONDS,
+        SECONDS,
+        
+        COUNT 
+    };
+    unsigned long long mStart;
+    unsigned long long mEnd;
+    UNIT mResultUnit;
+    
+    TimeMeasure( UNIT resultUnit = MILLIESECONDS )
+    {
+        this->Start();
+        this->mResultUnit = resultUnit;
     }
     
     ~TimeMeasure()
     {
-        unsigned long long end = this->GetCurrentMS();
-        std::cout << "TimeMeasure: " << end-this->mStart << " ms";
+        this->End();
+        this->PrintResult();
+    }
+    
+    unsigned long long
+    Start()
+    {
+        this->mStart = this->GetCurrentMS();
+        return this->mStart;
+    }
+    
+    unsigned long long
+    End()
+    {
+        this->mEnd = this->GetCurrentMS();
+        return this->mEnd;
+    }
+    
+    void
+    PrintResult()
+    {
+        unsigned long long resultInMS = this->mEnd-this->mStart;
+        switch(this->mResultUnit)
+        {
+            case MILLIESECONDS:
+            {
+                std::cout << "TimeMeasure: " << resultInMS << " ms" << std::endl;
+            }
+            break;
+            
+            case SECONDS:
+            {
+                std::cout << "TimeMeasure: " << resultInMS*0.001f << " s" << std::endl;
+            }
+            break;
+            
+            default:
+            {
+                std::cout << "TimeMeasure: Unhandeled mResultUnit" << std::endl;
+            }
+            break;
+        }
     }
     
     unsigned long long
@@ -71,9 +116,16 @@ fibonacci(unsigned int n)
 int main()
 {
     {
+        TimeMeasure tm(TimeMeasure::SECONDS);
+        {
+            std::cout << "f = " << fibonacci(10) << std::endl;
+        }
+    }
+    
+    {
         TimeMeasure tm;
         {
-            std::cout << "f(42) = " << fibonacci(44) << '\n';
+            std::cout << "f = " << fibonacci(35) << std::endl;
         }
     }
 
